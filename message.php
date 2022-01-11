11cb2ba93e45b74f68cdcd14ab9aadc524639fa1
Vision-Tey <nimusiimavision@gmail.com>
2022-01-11 22:48:42 +0300

    form

--- message.php
+++ message.php
@@ -1,21 +1,38 @@
 <?php
-     $name = $_POST['name'];
-     $email = $_POST['email'];
-     $phone = $_POST['phone'];
-     $message = $_POST['message'];
+
+
+
+    //  $name = $_POST['name'];
+    //  $email = $_POST['email'];
+    //  $phone = $_POST['phone'];
+    //  $message = $_POST['message'];
 
      
      
-    $to = "lordschildrenandelderly@gmail.com";
-    $subject = "From: $name <$email>";
-    $body = "Name: $name\nEmail: $email\nPhone: $phone\n\nMessage: $message\n\nRegards, \n$name";
-    $sender = "From: $email";
+    // $to = "lordschildrenandelderly@gmail.com";
+    // $subject = "From: $name <$email>";
+    // $body = "Name: $name\nEmail: $email\nPhone: $phone\n\nMessage: $message\n\nRegards, \n$name";
+    // $sender = "From: $email";
             
             
-    mail($to, $subject, $body, $sender)
+    // mail($to, $subject, $body, $sender)
 
-    header("Location: index.html");
+    // header("Location: index.html");
 
+
+
+    if(isset($_POST['sendd']))
+    {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $message = $_POST['message'];
+
+
+        $to = "lordschildrenandelderly@gmail.com";
+
+        header("location:index.html");
+    }
 
 
 
